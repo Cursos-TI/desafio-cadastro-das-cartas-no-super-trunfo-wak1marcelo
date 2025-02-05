fcab166170a5d0a5a443d50eca0f5cc73339e661
Marcelo Waki <151408097+wak1marcelo@users.noreply.github.com>
2025-02-05 03:50:33 +0000

    Desafio inciante super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,34 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    char codigo_da_cidade[5];
+    char cidade[20];
+    int populacao_da_cidade;
+    float area_da_cidade;
+    int qtade_pontos_turisticos;
+    
+    // Mensagem de boas vindas ao jogo
+    printf("Bem-vindo ao jogo super trunfo!!\n");
+    printf("Faça o cadastro das cartas do jogo\n");
+    
+    // Cadastro dos dados das cartas
+    printf("\nDigite o codigo da cidade: (EX: A01, A02, B01 ....)");
+    scanf("%s", &codigo_da_cidade);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &cidade);
+    printf("Digite a populacao da cidade: ");
+    scanf("%d", &populacao_da_cidade);
+    printf("Digite a area da cidade: ");
+    scanf("%f", &area_da_cidade);
+    printf("Digite o total de pontos turisticos da cidade: ");
+    scanf("%d", &qtade_pontos_turisticos);
+
+    // Exibicao dos dados armazenados acima
+    printf("\nCodigo da cidade: %s", codigo_da_cidade);
+    printf("\nNome da cidade: %s", cidade);
+    printf("\nPopulacao da cidade: %d", populacao_da_cidade);
+    printf("\nArea da cidade: %f", area_da_cidade);
+    printf("\nQuantidade de pontos turisticos da cidade: %d", qtade_pontos_turisticos);
+
     return 0;
 }
